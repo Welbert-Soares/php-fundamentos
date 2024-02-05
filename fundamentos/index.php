62e49d1ae514b006739b36f581c64338911e721f
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-05 15:13:29 -0300

    configurando sistema

---
+++ fundamentos/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+</head>
+
+<body>
+    <h1>
+        <?php 
+            print "Hello World!! \u{2615}";
+        ?>
+    </h1>
+    <p> I'm Welbert Soares, I live in Brazil/ Minas Gerais and I'm all about programming!! </p>
+</body>
+
+</html>

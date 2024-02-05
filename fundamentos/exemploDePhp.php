22cebcc0e00efc107669f21ab12a34f816aedd77
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-02-05 18:23:06 -0300

    exemplo de php

---
+++ fundamentos/exemploDePhp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    <h1>Exemplo de PHP</h1>
+    <?php 
+        
+    ?>
+</body>
+
+</html>
